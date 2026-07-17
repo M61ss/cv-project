8484475940854e02cabfcb751fe40aa2c6da3ab3
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-17 22:22:26 +0200

    restore non parallel working version

---
+++ UNet/dataset.py
@@ -0,0 +1,62 @@
+import os
+from glob import glob
+
+import torch
+
+from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ScaleIntensityd
+from monai.data import Dataset, DataLoader, list_data_collate
+
+
+train_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/train'
+validation_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
+
+train_images = sorted(glob(os.path.join(train_dir, 'images/*.png')))
+train_masks = sorted(glob(os.path.join(train_dir, 'masks/*.png')))
+validation_images = sorted(glob(os.path.join(validation_dir, 'images/*.png')))
+validation_masks = sorted(glob(os.path.join(validation_dir, 'masks/*.png')))
+
+train_files = [ {'img': img, 'mask': mask} for img, mask in zip(train_images, train_masks) ]
+validation_files = [ {'img': img, 'mask': mask} for img, mask in zip(validation_images, validation_masks) ]
+
+train_transforms = Compose(
+    [
+        LoadImaged(keys=['img', 'mask']),
+        EnsureChannelFirstd(keys=['img', 'mask']),
+        ScaleIntensityd(keys=['img']),
+    ]
+)
+validation_transforms = Compose(
+    [
+        LoadImaged(keys=['img', 'mask']),
+        EnsureChannelFirstd(keys=['img', 'mask']),
+        ScaleIntensityd(keys=['img']),
+    ]
+)
+
+train_dataset = Dataset(
+    data=train_files,
+    transform=train_transforms,
+)
+validation_dataset = Dataset(
+    data=validation_files,
+    transform=validation_transforms,
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=64,
+    num_workers=4,
+    persistent_workers=True,
+    prefetch_factor=4,
+    pin_memory=torch.cuda.is_available(),
+    collate_fn=list_data_collate,
+)
+validation_loader = DataLoader(
+    validation_dataset,
+    batch_size=64,
+    num_workers=4,
+    persistent_workers=True,
+    prefetch_factor=4,
+    pin_memory=torch.cuda.is_available(),
+    collate_fn=list_data_collate,
+)
---
+++ UNet/main.py
@@ -0,0 +1,98 @@
+import logging
+import sys
+import os
+from math import ceil
+
+import torch
+from torch.optim import Adam
+from monai.networks.nets import UNet
+from monai.losses import DiceLoss
+
+import monai
+
+from dataset import train_loader, validation_loader
+
+
+monai.config.print_config()
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+checkpoint_dir = os.path.join(os.path.dirname(__file__), 'checkpoints')
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = UNet(
+    spatial_dims=2,
+    in_channels=1,
+    out_channels=1,
+    channels=(16, 32, 64, 128, 256),
+    strides=(2, 2, 2, 2),
+    num_res_units=2
+).to(device)
+
+loss_fun = DiceLoss(
+    sigmoid=True
+)
+
+optimizer = Adam(model.parameters(), lr=1e-4)
+
+N_EPOCHES = 100
+
+
+best_validation_loss = float('inf')
+best_validation_loss_epoch = -1
+for epoch in range(N_EPOCHES):
+    print("=" * 20)
+    print(f"EPOCH {epoch + 1}/{N_EPOCHES}")
+        
+    train_num_batches = ceil(len(train_loader.dataset) / float(train_loader.batch_size))
+
+    epoch_train_loss = 0
+    for i, train_batch in enumerate(train_loader):
+        train_imgs, train_masks = train_batch['img'].to(device), train_batch['mask'].to(device)
+        optimizer.zero_grad()
+        pred = model(train_imgs)
+        loss = loss_fun(pred, train_masks)
+        loss.backward()
+        optimizer.step()
+        epoch_train_loss += loss.item()
+        print(f"\t{i}/{train_num_batches} train_loss: {loss.item():.4f}")
+    epoch_train_loss /= train_num_batches
+    print(f"\tAverage train loss: {epoch_train_loss:.4f}")
+
+    with torch.no_grad():
+        validation_num_batches = ceil(len(validation_loader.dataset) / float(validation_loader.batch_size))
+
+        epoch_validation_loss = 0
+        for validation_batch in validation_loader:
+            validation_imgs, validation_masks = validation_batch['img'].to(device), validation_batch['mask'].to(device)
+            pred = model(validation_imgs)
+            loss = loss_fun(pred, validation_masks)
+            epoch_validation_loss += loss.item()
+        epoch_validation_loss /= validation_num_batches
+        print(f"\tAverage validation loss: {epoch_validation_loss:.4f}")
+
+        # ADD HERE EARLY STOPPING
+
+        if epoch_validation_loss < best_validation_loss:
+            best_validation_loss = epoch_validation_loss
+            best_validation_loss_epoch = epoch + 1
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': epoch_train_loss,
+                'validation_loss': epoch_validation_loss,
+            }, os.path.join(checkpoint_dir, 'best.pth'))
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'train_loss': epoch_train_loss,
+            'validation_loss': epoch_validation_loss,
+        }, os.path.join(checkpoint_dir, 'last.pth'))
+
+print(f"TRAIN COMPLETED!")
+print(f"best_metric: {best_validation_loss:.4f} at epoch: {best_validation_loss_epoch}")
