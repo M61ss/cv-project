b2a830cdaf8d30aedef3cda03a59a027de361334
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-22 10:06:57 +0200

    refactor

--- UNet/dataset.py
+++ UNet/dataset.py
@@ -8,15 +8,15 @@ from monai.data import Dataset, DataLoader, list_data_collate
 
 
 train_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/train'
-validation_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
+val_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
 
 train_images = sorted(glob(os.path.join(train_dir, 'images/*.png')))
 train_masks = sorted(glob(os.path.join(train_dir, 'masks/*.png')))
-validation_images = sorted(glob(os.path.join(validation_dir, 'images/*.png')))
-validation_masks = sorted(glob(os.path.join(validation_dir, 'masks/*.png')))
+val_images = sorted(glob(os.path.join(val_dir, 'images/*.png')))
+val_masks = sorted(glob(os.path.join(val_dir, 'masks/*.png')))
 
 train_files = [ {'img': img, 'mask': mask} for img, mask in zip(train_images, train_masks) ]
-validation_files = [ {'img': img, 'mask': mask} for img, mask in zip(validation_images, validation_masks) ]
+val_files = [ {'img': img, 'mask': mask} for img, mask in zip(val_images, val_masks) ]
 
 train_transforms = Compose(
     [
@@ -25,7 +25,7 @@ train_transforms = Compose(
         ScaleIntensityd(keys=['img']),
     ]
 )
-validation_transforms = Compose(
+val_transforms = Compose(
     [
         LoadImaged(keys=['img', 'mask']),
         EnsureChannelFirstd(keys=['img', 'mask']),
@@ -37,9 +37,9 @@ train_dataset = Dataset(
     data=train_files,
     transform=train_transforms,
 )
-validation_dataset = Dataset(
-    data=validation_files,
-    transform=validation_transforms,
+val_dataset = Dataset(
+    data=val_files,
+    transform=val_transforms,
 )
 
 train_loader = DataLoader(
@@ -52,8 +52,8 @@ train_loader = DataLoader(
     pin_memory=torch.cuda.is_available(),
     collate_fn=list_data_collate,
 )
-validation_loader = DataLoader(
-    validation_dataset,
+val_loader = DataLoader(
+    val_dataset,
     batch_size=64,
     num_workers=8,
     persistent_workers=True,
