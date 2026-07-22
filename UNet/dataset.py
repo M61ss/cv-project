483d07182d2139c16386be54efdc3d0aa94cf5f7
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-22 17:22:36 +0200

    configure test dataset

--- UNet/dataset.py
+++ UNet/dataset.py
@@ -9,14 +9,18 @@ from monai.data import Dataset, DataLoader, list_data_collate
 
 train_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/train'
 val_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
+test_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/test'
 
 train_images = sorted(glob(os.path.join(train_dir, 'images/*.png')))
 train_masks = sorted(glob(os.path.join(train_dir, 'masks/*.png')))
 val_images = sorted(glob(os.path.join(val_dir, 'images/*.png')))
 val_masks = sorted(glob(os.path.join(val_dir, 'masks/*.png')))
+test_images = sorted(glob(os.path.join(test_dir, 'images/*.png')))
+test_masks = sorted(glob(os.path.join(test_dir, 'masks/*.png')))
 
 train_files = [ {'img': img, 'mask': mask} for img, mask in zip(train_images, train_masks) ]
 val_files = [ {'img': img, 'mask': mask} for img, mask in zip(val_images, val_masks) ]
+test_files = [ {'img': img, 'mask': mask} for img, mask in zip(test_images, test_masks) ]
 
 train_transforms = Compose(
     [
@@ -32,6 +36,13 @@ val_transforms = Compose(
         ScaleIntensityd(keys=['img']),
     ]
 )
+test_transforms = Compose(
+    [
+        LoadImaged(keys=['img', 'mask']),
+        EnsureChannelFirstd(keys=['img', 'mask']),
+        ScaleIntensityd(keys=['img']),
+    ]
+)
 
 train_dataset = Dataset(
     data=train_files,
@@ -40,6 +51,10 @@ train_dataset = Dataset(
 val_dataset = Dataset(
     data=val_files,
     transform=val_transforms,
+)
+test_dataset = Dataset(
+    data=test_files,
+    transform=test_transforms
 )
 
 train_loader = DataLoader(
@@ -54,6 +69,15 @@ train_loader = DataLoader(
 )
 val_loader = DataLoader(
     val_dataset,
+    batch_size=64,
+    num_workers=8,
+    persistent_workers=True,
+    prefetch_factor=4,
+    pin_memory=torch.cuda.is_available(),
+    collate_fn=list_data_collate,
+)
+test_loader = DataLoader(
+    test_dataset,
     batch_size=64,
     num_workers=8,
     persistent_workers=True,
