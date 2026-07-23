d5ca8090402c242a0f925dabbb9aa4712c956d0f
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-23 12:25:46 +0200

    split correctly dataset

--- UNet/dataset.py
+++ UNet/dataset.py
@@ -2,69 +2,76 @@ import os
 from glob import glob
 
 import torch
+from torch.utils.data.dataset import random_split
 
 from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ScaleIntensityd
 from monai.data import Dataset, DataLoader, list_data_collate
 
 
-train_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/train'
-val_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
-test_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/test'
+data_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/train'
+# val_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/validation'
+# test_dir = '/work/cvcs2026/LZMM/OpenEDS/openEDS/openEDS/test'
 
-train_images = sorted(glob(os.path.join(train_dir, 'images/*.png')))
-train_masks = sorted(glob(os.path.join(train_dir, 'masks/*.png')))
-val_images = sorted(glob(os.path.join(val_dir, 'images/*.png')))
-val_masks = sorted(glob(os.path.join(val_dir, 'masks/*.png')))
-test_images = sorted(glob(os.path.join(test_dir, 'images/*.png')))
-test_masks = sorted(glob(os.path.join(test_dir, 'masks/*.png')))
+images = sorted(glob(os.path.join(data_dir, 'images/*.png')))
+masks = sorted(glob(os.path.join(data_dir, 'masks/*.png')))
+# val_images = sorted(glob(os.path.join(val_dir, 'images/*.png')))
+# val_masks = sorted(glob(os.path.join(val_dir, 'masks/*.png')))
+# test_images = sorted(glob(os.path.join(test_dir, 'images/*.png')))
+# test_masks = sorted(glob(os.path.join(test_dir, 'masks/*.png')))
 
-train_files = [ {'img': img, 'mask': mask} for img, mask in zip(train_images, train_masks) ]
-val_files = [ {'img': img, 'mask': mask} for img, mask in zip(val_images, val_masks) ]
-test_files = [ {'img': img, 'mask': mask} for img, mask in zip(test_images, test_masks) ]
+train_files = [ {'img': img, 'mask': mask} for img, mask in zip(images, masks) ]
+# val_files = [ {'img': img, 'mask': mask} for img, mask in zip(val_images, val_masks) ]
+# test_files = [ {'img': img, 'mask': mask} for img, mask in zip(test_images, test_masks) ]
 
-print('Train files:\t', len(train_files))
-print('Val files:\t', len(val_files))
-print('Test files:\t', len(test_files))
+print('Total images:\t', len(train_files))
+# print('Val files:\t', len(val_files))
+# print('Test files:\t', len(test_files))
 
-train_transforms = Compose(
+transforms = Compose(
     [
         LoadImaged(keys=['img', 'mask']),
         EnsureChannelFirstd(keys=['img', 'mask']),
         ScaleIntensityd(keys=['img']),
     ]
 )
-val_transforms = Compose(
-    [
-        LoadImaged(keys=['img', 'mask']),
-        EnsureChannelFirstd(keys=['img', 'mask']),
-        ScaleIntensityd(keys=['img']),
-    ]
-)
-test_transforms = Compose(
-    [
-        LoadImaged(keys=['img', 'mask']),
-        EnsureChannelFirstd(keys=['img', 'mask']),
-        ScaleIntensityd(keys=['img']),
-    ]
-)
+# val_transforms = Compose(
+#     [
+#         LoadImaged(keys=['img', 'mask']),
+#         EnsureChannelFirstd(keys=['img', 'mask']),
+#         ScaleIntensityd(keys=['img']),
+#     ]
+# )
+# test_transforms = Compose(
+#     [
+#         LoadImaged(keys=['img', 'mask']),
+#         EnsureChannelFirstd(keys=['img', 'mask']),
+#         ScaleIntensityd(keys=['img']),
+#     ]
+# )
 
-train_dataset = Dataset(
+dataset = Dataset(
     data=train_files,
-    transform=train_transforms,
+    transform=transforms,
 )
-val_dataset = Dataset(
-    data=val_files,
-    transform=val_transforms,
-)
-test_dataset = Dataset(
-    data=test_files,
-    transform=test_transforms
-)
+# val_dataset = Dataset(
+#     data=val_files,
+#     transform=val_transforms,
+# )
+# test_dataset = Dataset(
+#     data=test_files,
+#     transform=test_transforms
+# )
+
+train_dataset, val_dataset, test_dataset = random_split(dataset, [0.8, 0.1, 0.1])
+
+print('Train images:\t', len(train_dataset))
+print('Val images:\t', len(val_dataset))
+print('Test images:\t', len(test_dataset))
 
 train_dl = DataLoader(
     train_dataset,
     batch_size=64,
-    num_workers=8,
+    num_workers=12,
     persistent_workers=True,
     prefetch_factor=4,
     shuffle=True,
@@ -74,9 +81,9 @@ train_dl = DataLoader(
 val_dl = DataLoader(
     val_dataset,
     batch_size=64,
-    num_workers=8,
+    num_workers=6,
     persistent_workers=True,
-    prefetch_factor=4,
+    prefetch_factor=2,
     pin_memory=torch.cuda.is_available(),
     collate_fn=list_data_collate,
 )
@@ -84,8 +91,8 @@ test_dl = DataLoader(
     test_dataset,
     batch_size=64,
     num_workers=8,
-    persistent_workers=True,
-    prefetch_factor=4,
+    persistent_workers=False,
+    prefetch_factor=2,
     pin_memory=torch.cuda.is_available(),
     collate_fn=list_data_collate,
 )
