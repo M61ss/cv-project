147672caab5e758b4d7d6afbef0b04caf5334cc8
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-26 14:59:05 +0200

    complete test script

--- UNet/test.py
+++ UNet/test.py
@@ -6,7 +6,7 @@ import torch
 from monai.networks.nets import UNet
 from monai.losses import DiceLoss
 
-from config import config
+from config import run
 from dataset import test_dl
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -15,12 +15,12 @@ checkpoint = torch.load(os.path.join(os.path.dirname(__file__), 'checkpoints/bes
 
 model = UNet(
     spatial_dims=2,
-    in_channels=config['input_channels'],
-    out_channels=config['output_channels'],
-    channels=config['channels'],
-    strides=config['strides'],
-    num_res_units=config['num_res_units'],
-    dropout=config['dropout']
+    in_channels=run.config['input_channels'],
+    out_channels=run.config['output_channels'],
+    channels=run.config['channels'],
+    strides=run.config['strides'],
+    num_res_units=run.config['num_res_units'],
+    dropout=run.config['dropout']
 ).to(device)
 
 model.load_state_dict(checkpoint['model_state_dict'])
@@ -31,18 +31,21 @@ loss_fun = DiceLoss(
     softmax=True
 )
 
-with torch.no_grad():
-    test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
-    test_loss = 0
-    pupil_accuracy = 0
-    for test_batch in test_dl:
-        test_imgs = test_batch['img'].to(device)
-        test_masks = test_batch['mask'].to(device)
-        test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
+with run:
+    with torch.no_grad():
+        test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
+        test_loss = 0
+        pupil_accuracy = 0
+        for test_batch in test_dl:
+            test_imgs = test_batch['img'].to(device)
+            test_masks = test_batch['mask'].to(device)
+            test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
 
-        pred_labels = model(test_imgs)
-        loss = loss_fun(pred_labels, test_labels)
-        test_loss += loss.item()
+            pred_labels = model(test_imgs)
+            loss = loss_fun(pred_labels, test_labels)
+            test_loss += loss.item()
 
-    test_loss /= test_num_batches
-    print(f"\tAverage test loss: {test_loss:.4f}")
+        test_loss /= test_num_batches
+        print('@' * 20)
+        print(f"Average test loss: {test_loss:.4f}")
+        print('@' * 20)
