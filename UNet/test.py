10de5f08fd9a31acc80375bee3666708c2f5e470
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-26 16:33:54 +0200

    correct test script

--- UNet/test.py
+++ UNet/test.py
@@ -11,7 +11,7 @@ from dataset import test_dl
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-checkpoint = torch.load(os.path.join(os.path.dirname(__file__), 'checkpoints/best.pth'))
+checkpoint = torch.load(os.path.join(os.path.dirname(__file__), f'checkpoints/best-{run.name}.pth'))
 
 model = UNet(
     spatial_dims=2,
@@ -31,21 +31,20 @@ loss_fun = DiceLoss(
     softmax=True
 )
 
-with run:
-    with torch.no_grad():
-        test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
-        test_loss = 0
-        pupil_accuracy = 0
-        for test_batch in test_dl:
-            test_imgs = test_batch['img'].to(device)
-            test_masks = test_batch['mask'].to(device)
-            test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
+with torch.no_grad():
+    test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
+    test_loss = 0
+    pupil_accuracy = 0
+    for test_batch in test_dl:
+        test_imgs = test_batch['img'].to(device)
+        test_masks = test_batch['mask'].to(device)
+        test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
 
-            pred_labels = model(test_imgs)
-            loss = loss_fun(pred_labels, test_labels)
-            test_loss += loss.item()
+        pred_labels = model(test_imgs)
+        loss = loss_fun(pred_labels, test_labels)
+        test_loss += loss.item()
 
-        test_loss /= test_num_batches
-        print('@' * 20)
-        print(f"Average test loss: {test_loss:.4f}")
-        print('@' * 20)
+    test_loss /= test_num_batches
+    print('@' * 20)
+    print(f"Average test loss: {test_loss:.4f}")
+    print('@' * 20)
