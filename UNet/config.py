f23e3937e9c7da2f2c14c33cbeeb1fe8ca6c28c5
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-26 19:35:25 +0200

    config refactoring

--- UNet/config.py
+++ UNet/config.py
@@ -9,6 +9,23 @@ import monai
 import wandb
 
 
+wandb.login()
+wandb_project_name = "UNet"
+wandb_config = {
+    "dataset": "OpenEDS-blur-augmented",
+    'architecture': 'UNet',
+    'input_channels': 1,
+    'output_channels': 4,
+    'channels': (8, 16, 32, 64, 128),
+    'strides': (2, 2, 2, 2),
+    'num_res_units': 3,
+    'dropout': 0.4,
+    'learning_rate': 1e-4,
+    "epoches": 100
+}
+
+run = wandb.init(project=wandb_project_name, config=wandb_config)
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 monai.config.print_config()
@@ -17,20 +34,3 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 checkpoint_dir = os.path.join(os.path.dirname(__file__), 'checkpoints')
 os.makedirs(checkpoint_dir, exist_ok=True)
-
-wandb.login()
-wandb_project_name = "UNet"
-wandb_config = {
-    "dataset": "OpenEDS-blur-augmented",
-    'architecture': 'UNet',
-    'input_channels': 1,
-    'output_channels': 4,
-    'channels': (8, 16, 32, 64),
-    'strides': (2, 2, 2),
-    'num_res_units': 3,
-    'dropout': 0.0,
-    'learning_rate': 1e-4,
-    "epoches": 100
-}
-
-run = wandb.init(project=wandb_project_name, config=wandb_config)
