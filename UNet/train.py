b128f41d224873c7b7d3901b45db5fbbace84f01
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-28 16:46:33 +0200

    add test in train script

--- UNet/train.py
+++ UNet/train.py
@@ -7,7 +7,7 @@ from monai.networks.nets import UNet
 from monai.losses import DiceLoss
 
 from config import run, device, checkpoint_dir
-from openeds import train_dl, val_dl
+from openeds import train_dl, val_dl, test_dl
 from earlystopper import EarlyStopping
 
 
@@ -106,3 +106,22 @@ with run:
 
     print(f"TRAIN COMPLETED!")
     print(f"best_metric: {best_val_loss:.4f} at epoch: {best_val_loss_epoch}")
+
+
+with torch.no_grad():
+    test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
+    test_loss = 0
+    pupil_accuracy = 0
+    for test_batch in test_dl:
+        test_imgs = test_batch['img'].to(device)
+        test_masks = test_batch['mask'].to(device)
+        test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
+
+        pred_labels = model(test_imgs)
+        loss = loss_fun(pred_labels, test_labels)
+        test_loss += loss.item()
+
+    test_loss /= test_num_batches
+    print('@' * 20)
+    print(f"Average test loss: {test_loss:.4f}")
+    print('@' * 20)
