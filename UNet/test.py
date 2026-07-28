f61686a1f6554339dcf14ca2e8e2d405bbaae3f6
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-28 17:40:43 +0200

    correct quotes

--- UNet/test.py
+++ UNet/test.py
@@ -34,15 +34,15 @@ def test_model(model: UNet, loss_fun):
         print(f"Average test loss: {test_loss:.4f}")
         print('-' * 20)
         print('Dice metric:')
-        print(f'\t- mean:\t\t{dice_metric.aggregate(reduction='mean')}')
-        print(f'\t- mean_batch:\t\t{dice_metric.aggregate(reduction='mean_batch')}')
-        print(f'\t- mean_channel:\t{dice_metric.aggregate(reduction='mean_channel')}')
+        print(f'\t- mean:\t\t{dice_metric.aggregate(reduction="mean")}')
+        print(f'\t- mean_batch:\t\t{dice_metric.aggregate(reduction="mean_batch")}')
+        print(f'\t- mean_channel:\t{dice_metric.aggregate(reduction="mean_channel")}')
         print('-' * 20)
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-checkpoint = torch.load(os.path.join(os.path.dirname(__file__), f'checkpoints/best-.pth'))
+checkpoint = torch.load(os.path.join(os.path.dirname(__file__), f'checkpoints/blur-mask-aug.pth'))
 
 model = UNet(
     spatial_dims=2,
