23c5dd2366fb6a7dd9b9749efb91eab5e7ae23ce
Mattia Massarenti <317651@studenti.unimore.it>
2026-07-28 17:25:17 +0200

    move testing to a function and add some metrics

--- UNet/test.py
+++ UNet/test.py
@@ -5,8 +5,38 @@ import torch
 
 from monai.networks.nets import UNet
 from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
 
 from openeds import test_dl
+
+
+def test_model(model: UNet):
+    dice_metric = DiceMetric(
+        num_classes=model.out_channels
+    )
+    
+    with torch.no_grad():
+        test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
+        test_loss = 0
+        for test_batch in test_dl:
+            test_imgs = test_batch['img'].to(device)
+            test_masks = test_batch['mask'].to(device)
+            test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
+
+            pred_labels = model(test_imgs)
+            loss = loss_fun(pred_labels, test_labels)
+            test_loss += loss.item()
+
+        test_loss /= test_num_batches
+        print('-' * 20)
+        print(f"Average test loss: {test_loss:.4f}")
+        print('-' * 20)
+        print('Dice metric:')
+        print(f'\t- mean:\t\t{dice_metric.aggregate(reduction='mean')}')
+        print(f'\t- mean_batch:\t\t{dice_metric.aggregate(reduction='mean_batch')}')
+        print(f'\t- mean_channel:\t{dice_metric.aggregate(reduction='mean_channel')}')
+        print('-' * 20)
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -30,20 +60,4 @@ loss_fun = DiceLoss(
     softmax=True
 )
 
-with torch.no_grad():
-    test_num_batches = ceil(len(test_dl.dataset) / float(test_dl.batch_size))
-    test_loss = 0
-    pupil_accuracy = 0
-    for test_batch in test_dl:
-        test_imgs = test_batch['img'].to(device)
-        test_masks = test_batch['mask'].to(device)
-        test_labels = test_batch['label'].permute(0, 1, 3, 2).to(device)
-
-        pred_labels = model(test_imgs)
-        loss = loss_fun(pred_labels, test_labels)
-        test_loss += loss.item()
-
-    test_loss /= test_num_batches
-    print('@' * 20)
-    print(f"Average test loss: {test_loss:.4f}")
-    print('@' * 20)
+test_model(model)
