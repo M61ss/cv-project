1b9d650b63e5bb45ea514620affd30ae31a8167d
Mattia Massarenti <317651@studenti.unimore.it>
2026-09-18 21:46:33 +0200

    implement lstm

--- RNN/lstm.py
+++ RNN/lstm.py
@@ -1,8 +1,13 @@
-import torch
-from torch.nn import LSTM
+import torch.nn as nn
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print('Device:', device)
+class LightLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, out_size, num_layers=1):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, out_size)
 
-model = LSTM(1, 16, 2)
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
